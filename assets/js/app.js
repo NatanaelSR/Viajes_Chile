679fad5dac79552287304e6674c43cef05566b90
Natanael <natanael.soto.rodriguez@gmail.com>
2026-06-26 20:28:04 -0400

    se añade autorun del carousel y ajusta tamaño para pantallas mas grandes. Se actualiza css y js.Se añaden comentarios en css y js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,40 @@
+// Inicializacion de tooltip
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-document.addEventListener('DOMContentLoaded', function() {
+
+// Se espera a que carge el documento
+document.addEventListener('DOMContentLoaded', () =>{
     const formulario = document.getElementById('formularioContacto');
-    
-    formulario.addEventListener('submit', function(event) {
+
+//Alerta de envio de datos con modal de Bootstrap y reseteo del formulario
+    formulario.addEventListener('submit', (event) => {
         event.preventDefault();
 
         const modalExito = new bootstrap.Modal(document.getElementById('modalExito'));
         modalExito.show();
 
-        formulario.reset();
+        formulario.reset();        
     });
 });
 
-
-
+// Se inicia autorun del carusel cada 5 segundos
+const carruselElement = document.getElementById('carouselViajeschile');
+ const bsCarousel = bootstrap.Carousel.getOrCreateInstance(carruselElement, {
+        ride: 'carousel',    
+        interval: 5000       
+        });
+    
+//Se inicia Jquery
 $(() => {
     console.log('Entorno de jQuery inicializado.');
 
-     
- $('.mod-color').on('dblclick', function () {
+//Se cambia de color con doble click texto de Quienes Somos     
+ $('.mod-color').on('dblclick',  ()=> {
         $(this).toggleClass('active');
 });
-
+//Se esconde y aparece seccion de Destacados
  $('#toggle-destacados').on('click', () => {
         $('#toggle-cards').slideToggle(800);
     });
-        
     });
